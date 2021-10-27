e55421a7e1d13f75f8c869684e05d2f5f44c0c8f
JuniorAndradePinheiro <junior.fenix001@gmail.com>
2021-10-27 20:01:09 -0300

    Versao 1.8

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,15 +1,16 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
+import java.util.function.Predicate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -22,57 +23,57 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries{
 
 	@PersistenceContext
 	EntityManager manager;
-	
-	
-//essa forma é interessante mas se for alterado o nome do método aqui ou no repositório não vai apontar um erro de execução.
-//por isso o ideal é extrair uma interface desta classe fazer o repositório herda-la (é permitido herança multipla no caso de interface)
-	
+		
+//Desta forma é possivel realizar consultas dinamicas com a mesma query, passando varios parametros, somente um ou nenhum, que a query vai sendo montada conforme os parametros são preenchidos	
 //	@Override
 //	public List<Restaurante> find(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal){
 //		
-//		String jpql = "from Restaurante whrere nome like :nome and taxaFrete between: taxaInicial and taxaFinal";
-//		//aqui os % % não funciona no JPQL puro, por isso foi colocado nos "setParameter"
+//		//StringBuilder é uma classe utilitária para manipulação de String
+//		var jpql = new StringBuilder("from Restaurante whrere 0=0 ");
+//		var parametros = new HashMap<String, Object>();
 //		
-//		return manager.createQuery(jpql, Restaurante.class)
-//				.setParameter("nome", "%" + nome + "%")
-//				.setParameter("taxaInicial", "%" + taxaInicial + "%")
-//				.setParameter("taxaFinal", "%" +  taxaFinal + "%")
-//				.getResultList();
+//		//StringUtils é uma classe utilitária para manipulação de String
+//		//hasLength é um método que verfica se a String não é nula e se o comprimento dela não é 0
+//		if(StringUtils.hasLength(nome)) {
+//			jpql.append("and nome like :nome ");
+//			parametros.put("nome", "%" + nome + "%");
+//		}
+//		
+//		if(taxaInicial != null) {
+//			//.append é um método da classe StringBuilder para concaternar Strings
+//			jpql.append("and taxaFrete >= :taxaInicial ");
+//			parametros.put("taxaInicial", "%" + taxaInicial + "%");
+//		}
+//		
+//		if(taxaFinal !=null) {
+//			jpql.append("and taxaFrete <= :taxaFinal ");
+//			parametros.put("taxaFinal", "%" +  taxaFinal + "%");
+//		}
+//		
+//		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+//		
+//		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+//		
+//		return query.getResultList();
 //	}
 	
-	
-//Desta forma é possivel realizar consultas dinamicas com a mesma query, passando varios parametros, somente um ou nenhum, que a query vai sendo montada conforme os parametros são preenchidos	
+
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal){
+
+		//criteriaAPI é uma API que permite criar queries com código JAVA. Indicada somente para consultas muito complexas, esse método é mais burocrático.
 		
-		//StringBuilder é uma classe utilitária para manipulação de String
-		var jpql = new StringBuilder("from Restaurante whrere 0=0 ");
-		var parametros = new HashMap<String, Object>();
+		//É um criador de expressões SQL para o criteriQuery usar.
+		CriteriaBuilder builder = manager.getCriteriaBuilder(); 
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class); //nesse trecho informa a classe que você usará pra fazer a query
 		
-		//StringUtils é uma classe utilitária para manipulação de String
-		//hasLength é um método que verfica se a String não é nula e se o comprimento dela não é 0
-		if(StringUtils.hasLength(nome)) {
-			jpql.append("and nome like :nome ");
-			parametros.put("nome", "%" + nome + "%");
-		}
+		criteria.from(Restaurante.class); //Equivalente ao JPQL from Restaurante
 		
-		if(taxaInicial != null) {
-			//.append é um método da classe StringBuilder para concaternar Strings
-			jpql.append("and taxaFrete >= :taxaInicial ");
-			parametros.put("taxaInicial", "%" + taxaInicial + "%");
-		}
 		
-		if(taxaFinal !=null) {
-			jpql.append("and taxaFrete <= :taxaFinal ");
-			parametros.put("taxaFinal", "%" +  taxaFinal + "%");
-		}
-		
-		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
-		
-		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-		
+
+		//Usar uma sobrecarga do createQuery para criteria
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
-	}
-	
+	}	
 	
 }
